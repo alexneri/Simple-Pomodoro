5f6ca238798c3d1932d98b5e940f38911d65b169
Alexander Nicholas Neri <alexn@backbase.com>
2023-04-10 17:36:44 +0200

    Initial Commit

---
+++ Simple Pomodoro/Simple_PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_PomodoroApp.swift
+//  Simple Pomodoro
+//
+//  Created by Alexander Nicholas Neri on 10/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Simple_PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
